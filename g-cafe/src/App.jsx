f47f5bbe37a19117e6acb5f1fe47dc7e3a6bcfc2
Jardel Maciel <jardel.maciel22@gmail.com>
2025-08-30 18:50:50 -0300

    feat

---
+++ g-cafe/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='font-light text-amber-500'> Jardel Maciel </h1>
+        
+    </>
+  )
+}
+
+export default App
