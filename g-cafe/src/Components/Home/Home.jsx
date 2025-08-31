e6f46890388d1ee08c9f6a2eb0b2092b4f12d6a1
Jardel Maciel <jardel.maciel22@gmail.com>
2025-08-31 18:49:20 -0300

    Update Home.jsx

--- g-cafe/src/Components/Home/Home.jsx
+++ g-cafe/src/Components/Home/Home.jsx
@@ -2,7 +2,9 @@ import Botao from "../Botao/Botao"
 export default function Home(){
     return(
         <>
-        <h1 className="text-amber-50">Jardel Maciel</h1>
+        <h1 className="text-amber-50 pb-6">Jardel Maciel</h1>
+
+        <Botao label={"Jardel"} link={"http://www.google.com"}/>
         
         </>
     )
