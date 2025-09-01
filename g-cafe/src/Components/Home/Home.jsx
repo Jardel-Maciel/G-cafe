fd594e5e54c871b1be2e4adb6f886997af2a3f0f
Jardel Maciel <jardel.maciel22@gmail.com>
2025-09-01 11:20:52 -0300

    feat: navbar

--- g-cafe/src/Components/Home/Home.jsx
+++ g-cafe/src/Components/Home/Home.jsx
@@ -1,9 +1,28 @@
 import Botao from "../Botao/Botao"
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <>
-        <h1 className="text-amber-50">Jardel Maciel</h1>
-        
+            <header className="flex justify-around items-center pt-2.5">
+
+                <h1>G cafe</h1>
+
+                <nav className="flex justify-around items-center gap-4">
+                    <ul className="gap-7 flex">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Cardapio</a></li>
+                        <li><a href="#">Experiencia</a></li>
+                        <li><a href="#">Sobre</a></li>
+                    </ul>
+                    <Botao
+
+                        label={"Experimente"}
+                        link={"https://www.google.com"}
+
+
+                    />
+                </nav>
+            </header>
+
         </>
     )
 }
