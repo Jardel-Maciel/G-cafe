45e4ad19b36ade468b76320845e0e85b016cc2eb
Jardel Maciel <jardel.maciel22@gmail.com>
2025-09-05 00:03:57 -0300

    feat: routes

--- g-cafe/src/App.jsx
+++ g-cafe/src/App.jsx
@@ -2,6 +2,10 @@ import './App.css'
 import Botao from "./Components/Botao/Botao"
 import Home from "./Components/Home/Home"
 import Hero from "./Components/Home/Hero"
+import{BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import Sobre from "./pages/Sobre"
+import Contato from "./pages/Contato"
+import Homes from './pages/Homes'
 
 
 
@@ -10,8 +14,20 @@ function App() {
 
   return (
     <>
-      <Home />
-      <Hero />
+     
+      <Router>
+        <nav className='flex gap-4 p-4 justify-around bg-yellow-200'>
+          <Link to="/">Home</Link>
+          <Link to="/sobre">Sobre</Link>
+          <Link to="/contato">Contato</Link>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Homes />} />
+          <Route path='/sobre' element={<Sobre />} />
+          <Route path='/contato' element={<Contato />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ g-cafe/src/pages/Homes.jsx
@@ -0,0 +1,12 @@
+import Botao from '../components/botao/Botao';
+import botao from '../components/botao/Botao';
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Botao
+            label="Clique aqui"
+            />
+        </div>
+    )
+}
