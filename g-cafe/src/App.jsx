7227bd0c836dce48a4ee78c361385bb98db8b9af
Jardel Maciel <jardel.maciel22@gmail.com>
2025-09-05 00:41:11 -0300

    feat

--- g-cafe/src/App.jsx
+++ g-cafe/src/App.jsx
@@ -1,26 +1,30 @@
 import './App.css'
-import Botao from "./Components/Botao/Botao"
-import Home from "./Components/Home/Home"
-import Hero from "./Components/Home/Hero"
-import{BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Sobre from "./pages/Sobre"
 import Contato from "./pages/Contato"
 import Homes from './pages/Homes'
+import logo from './assets/Logo.png'
 
 
 
 function App() {
-  
+
 
   return (
     <>
-     
+
       <Router>
-        <nav className='flex gap-4 p-4 justify-around bg-yellow-200'>
-          <Link to="/">Home</Link>
-          <Link to="/sobre">Sobre</Link>
-          <Link to="/contato">Contato</Link>
-        </nav>
+        <header className='flex justify-around items-center pt-5 '>
+          
+          <img src={logo} alt=" logo marca" className='w-40 ' />
+         
+          <nav className=' gap-4 p-4 justify-around items-center md:flex hidden'>
+            
+            <Link to="/">Home</Link>
+            <Link to="/sobre">Sobre</Link>
+            <Link to="/contato">Contato</Link>
+          </nav>
+        </header>
 
         <Routes>
           <Route path='/' element={<Homes />} />
--- g-cafe/src/Components/Home/Hero.jsx
+++
@@ -1,8 +0,0 @@
-import imgBg from "../../assets/bg-hero.jpg"
-export default function Hero() {
-    return (
-        <section >
-            <img className="w-screen bg-cover" src={imgBg} alt="backgound" />
-        </section>
-    )
-}
